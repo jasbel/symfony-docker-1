2e6fdc68361232d2461984aa3abce880b1147bbe
jasbel <asbel.elmec@gmail.com>
2024-04-21 18:38:26 -0400

    feat: init

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+  #[Route('/lucky/number')]
+  public function number(): Response
+  {
+    $number = random_int(0, 100);
+
+    return new Response(
+      '<html><body>Lucky nunber : ' . $number . ' </body></html>'
+    );
+  }
+}
+
+?>
